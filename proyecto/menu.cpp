bdef9da7f552c683f23527a614bd458a260fe480
mcoronador <milagroscoronado16@gmail.com>
2026-07-12 09:49:05 -0500

    segunda parte del menu principal

--- proyecto/menu.cpp
+++ proyecto/menu.cpp
@@ -9,4 +9,8 @@ void mostrarMenuPrincipal() {
     cout << " [1] Gestionar TURNO: DESAYUNO\n";
     cout << " [2] Gestionar TURNO: ALMUERZO\n";
     cout << " [3] Gestionar TURNO: CENA\n";
+    cout << " [4] Ver Reportes Globales y Mermas del Dia\n";
+    cout << " [5] Salir del Sistema\n";
+    cout << "===========================================================\n";
+    cout << "Seleccione una opcion (1-5): ";
 }

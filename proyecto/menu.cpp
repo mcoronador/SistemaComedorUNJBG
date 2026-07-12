dcc0a2061ffe17955e92885fec8e7059ed1befc5
mcoronador <milagroscoronado16@gmail.com>
2026-07-12 09:47:21 -0500

    avance de menu principal

---
+++ proyecto/menu.cpp
@@ -0,0 +1,12 @@
+#include "menu.h"
+#include <iostream>
+using namespace std;
+
+void mostrarMenuPrincipal() {
+    cout << "===========================================================\n";
+    cout << "    COMEDOR-SGO - UNJBG                                    \n";
+    cout << "===========================================================\n";
+    cout << " [1] Gestionar TURNO: DESAYUNO\n";
+    cout << " [2] Gestionar TURNO: ALMUERZO\n";
+    cout << " [3] Gestionar TURNO: CENA\n";
+}

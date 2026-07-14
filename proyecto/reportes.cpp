be0f4d9004be7a3e0192cf1e663cd39fd4e75a3a
SebHuan-ns <ashuancam@unjbg.edu.pe>
2026-07-14 17:51:54 -0500

    Subiendo a proyecto

--- proyecto/reportes.cpp
+++ proyecto/reportes.cpp
@@ -13,8 +13,8 @@ int cierreHorarioYSaldos(int racionesTotales, int asistentes) {
     cout<<"========================================="<<endl;
     cout<<"|       CIERRE DE HORARIO Y SALDOS      |"<<endl;
     cout<<"========================================="<<endl;
-    cout<<"Raciones programadas totales: " <<racionesTotales<<endl;
-    cout<<"Alumnos que ya asistieron   : " <<asistentes<<endl;
+    cout<<"Raciones programadas totales: "<<racionesTotales<<endl;
+    cout<<"Alumnos que ya asistieron   : "<<asistentes<<endl;
     cout<<"-----------------------------------------"<<endl;
     cout<<" Saldo de Raciones Sobrantes : "<<racionesSobrantes<<endl;
     cout<<"========================================="<<endl;
@@ -22,10 +22,10 @@ int cierreHorarioYSaldos(int racionesTotales, int asistentes) {
     return racionesSobrantes;
 }
 
-void atenderCola(Alumno cola[], int &tamanoCola, int racionesSobrantes) {
+void atenderCola(Alumno cola[], int &tamanoCola, int racionesSobrantes){
     cout <<"\nIniciando proceso de atencion masiva de raciones sobrantes...\n"<<endl;
     
-    if (tamanoCola==0) {
+    if (tamanoCola==0){
         cout<<"No hay alumnos esperando en la cola virtual."<<endl;
         return;
     }
@@ -45,9 +45,9 @@ void atenderCola(Alumno cola[], int &tamanoCola, int racionesSobrantes) {
         atendidos++;
     }
     cout<<"========================================="<<endl;
-    cout<<"\n       RESUMEN DE PROCESO MASIVO       "<< endl;
-    cout <<"Alumnos atendidos con saldo : "<< atendidos<<endl;
-    cout <<"Alumnos restantes en cola   : "<< tamanoCola<<endl;
-    cout <<"Raciones sobrantes finales  : "<< racionesDisponibles<<endl;
-    cout <<"========================================"<<endl;
+    cout<<"\n       Datos finales                   "<<endl;
+    cout<<"Alumnos atendidos con saldo : "<<atendidos<<endl;
+    cout<<"Alumnos restantes en cola   : "<<tamanoCola<<endl;
+    cout<<"Raciones sobrantes finales  : "<<racionesDisponibles<<endl;
+    cout<<"========================================="<<endl;
 }
